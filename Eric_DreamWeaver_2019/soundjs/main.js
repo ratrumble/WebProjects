765969419a28693f48a75297dab76bb5602b9e2e
ratrumble <61366179+ratrumble@users.noreply.github.com>
2024-02-19 00:48:29 -0600

    Initial commit

---
+++ Eric_DreamWeaver_2019/soundjs/main.js
@@ -0,0 +1,25 @@
+// JavaScript Document
+	function chicken(){
+		 document.getElementById("audio").innerHTML="<embed src='sound/chicken.mp3' hidden='true' autostart='true' loop='false' />";
+	}
+	
+	function cow(){
+		 document.getElementById("audio").innerHTML="<embed src='sound/cow.mp3' hidden='true' autostart='true' loop='false' />";
+	}
+	
+	function goat(){
+		 document.getElementById("audio").innerHTML="<embed src='sound/goat.mp3' hidden='true' autostart='true' loop='false' />";
+	}
+	
+	function lamb(){
+		 document.getElementById("audio").innerHTML="<embed src='sound/lamb.mp3' hidden='true' autostart='true' loop='false' />";
+	}
+	
+	function pig(){
+		 document.getElementById("audio").innerHTML="<embed src='sound/pig.mp3' hidden='true' autostart='true' loop='false' />";
+	}
+	
+	function turkey(){
+		 document.getElementById("audio").innerHTML="<embed src='sound/turkey.mp3' hidden='true' autostart='true' loop='false' />";
+	}
+	
